034457a6bb1cfbc6d21b93d97bb76f5688b17e3e
Ognyan Manchev <ognyan.manchev@gmail.com>
2025-12-25 23:23:38 +0200

    6.7

--- Work/follow.py
+++ Work/follow.py
@@ -16,10 +16,13 @@ def follow(filename: str):
 
 
 if __name__ == "__main__":
+    import report
+
+    pf = report.read_portfolio("Data/portfolio.csv")
     for line in follow("Data/stocklog.csv"):
         fields = line.split(",")
         name = fields[0].strip('"')
         price = float(fields[1])
         change = float(fields[4])
-        if change < 0:
+        if name in pf:
             print(f"{name:>10s} {price:>10.2f} {change:>10.2f}")
