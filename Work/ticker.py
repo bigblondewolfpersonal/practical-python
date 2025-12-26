b76ea9357f4bb298274684e09d57c941e63f7a1c
Ognyan Manchev <ognyan.manchev@gmail.com>
2025-12-26 17:33:45 +0200

    6.11

---
+++ Work/ticker.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import csv
+
+from follow import follow
+import report
+
+def select_columns(rows, indices):
+    for row in rows:
+        yield [row[index] for index in indices]
+
+def convert_types(rows, types):
+    for row in rows:
+        yield [func(val) for func, val in zip(types, row)]
+
+def make_dicts(rows, headers):
+    for row in rows:
+        yield(dict(zip(headers, row)))
+
+def parse_stock_data(lines):
+    rows = csv.reader(lines)
+    rows = select_columns(rows, [0, 1, 4])
+    rows = convert_types(rows, [str, float, float])
+    rows = make_dicts(rows, ["name", "price", "change"])
+    return rows
+
+def filter_symbols(rows, names):
+    for row in rows:
+        if row["name"] in names:
+            yield row
+
+if __name__ == "__main__":
+    pf = report.read_portfolio("Data/portfolio.csv")
+    lines = follow("Data/stocklog.csv")
+    rows = parse_stock_data(lines)
+    rows = filter_symbols(rows, pf)
+    for row in rows:
+        print(row)
